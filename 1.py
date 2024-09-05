205edfa21c40b597ee50e7b372268c29965a273d
romangatiatullin <mr.artemka02@mail.ru>
2024-09-05 15:38:16 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks = "12"
+number_of_hours_spent = "1.5"
+course_name = "Python"
+time_for_one_task = "0.125"
+print("Курс:"+course_name+",", "всего задач:"+number_of_completed_tasks+",","затрачено часов:",number_of_hours_spent+",", "среднее время выполнения",time_for_one_task,"часа." )
